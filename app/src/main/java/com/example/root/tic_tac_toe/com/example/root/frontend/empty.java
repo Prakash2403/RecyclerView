6ac98b35926ef1270960dd7332881fa5cb711e1d
Prakash2403 <rishurai24@gmail.com>
2017-01-13 15:28:04 +0530

    -Created directory structure.

---
+++ app/src/main/java/com/example/root/tic_tac_toe/com/example/root/frontend/empty.java
@@ -0,0 +1,8 @@
+package com.example.root.tic_tac_toe.com.example.root.frontend;
+
+/**
+ * Created by root on 13/1/17.
+ */
+
+public class empty {
+}
---
+++ app/src/main/java/com/example/root/tic_tac_toe/empty.java
@@ -0,0 +1,10 @@
+package com.example.root.tic_tac_toe;
+
+/**
+ * Created by root on 13/1/17.
+ */
+
+public class empty
+{
+}
+
