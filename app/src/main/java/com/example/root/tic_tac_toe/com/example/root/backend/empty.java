aa745c3600262f7969bd2ef2d8b1d128059bc775
Piyush Sikarwal <sikarwal.ps@gmail.com>
2017-02-16 03:39:59 +0530

    - Reduced Title Font Size.

--- app/src/main/java/com/example/root/tic_tac_toe/com/example/root/backend/empty.java
+++
@@ -1,8 +0,0 @@
-package com.example.root.tic_tac_toe.com.example.root.backend;
-
-/**
- * Created by root on 13/1/17.
- */
-
-public class empty {
-}
--- app/src/main/java/com/example/root/tic_tac_toe/com/example/root/uiinterface/empty.java
+++
@@ -1,8 +0,0 @@
-package com.example.root.tic_tac_toe.com.example.root.uiinterface;
-
-/**
- * Created by root on 13/1/17.
- */
-
-public class empty {
-}
